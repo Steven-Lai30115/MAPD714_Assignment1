42167442c1c93ebba00bb25e60f4331a55230b73
Steven Lai <wai30115@live.hk>
2022-09-18 21:13:27 -0400

    initial commit

---
+++ MAPD714_Assignment1/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MAPD714_Assignment1
+//
+//  Created by  Pui Yan Cheung (301252393), Man Nok PUN (301269138), chin wai lai(301257824).
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
