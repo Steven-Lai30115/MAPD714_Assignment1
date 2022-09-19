f2e9aa69ed873c5ae31303486aaf31ce0933c157
Steven Lai <wai30115@live.hk>
2022-09-18 21:30:05 -0400

    Added style to interface

--- MAPD714_Assignment1/ViewController.swift
+++ MAPD714_Assignment1/ViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 
     
-
+    
+    @IBOutlet var displayArea: UIView!
+    @IBOutlet var buttonStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        styleDisplayArea()
+        styleAllButton()
+    }
+
+    // rounded effect on display area
+    func styleDisplayArea() {
+        displayArea.layer.cornerRadius = 20
+        displayArea.layer.shadowRadius = 10
+//        displayArea.layer.shadowOpacity = 1.0
+//        displayArea.layer.shadowOffset = CGSize(width: -3, height: 3)
+    }
+
+    // turn all rect button to cicle button
+    func styleAllButton() {
+        for case let stackView as UIStackView in buttonStackView.subviews {
+            for case let button as UIButton in stackView.subviews {
+                button.layer.cornerRadius = button.frame.width / 2
+                button.layer.masksToBounds = true
+            }
+        }
     }
 }
-
