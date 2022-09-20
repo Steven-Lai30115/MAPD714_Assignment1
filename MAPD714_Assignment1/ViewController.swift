39dcd516ea9406a1f4fb3427137ab370c96aa10f
Steven Lai <wai30115@live.hk>
2022-09-19 21:54:35 -0400

    Removed unuse Image & changed to white background

--- MAPD714_Assignment1/ViewController.swift
+++ MAPD714_Assignment1/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  MAPD714_Assignment1
-//
+//  Calculator App
 //  Created by  Pui Yan Cheung (301252393), Man Nok PUN (301269138), Chin Wai Lai(301257824).
 //  Last modified 15:28 19 Sept 2022
 //
