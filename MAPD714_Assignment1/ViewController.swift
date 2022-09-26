003a2ac839d3eb26fdc9491d4f5bd8413d38387c
ggsonson <113072348+ggsonson@users.noreply.github.com>
2022-09-26 14:12:13 -0400

    Add ButtonPressed functions
    Press buttons to input (No validation, incomplete for +/- and equal)

--- MAPD714_Assignment1/ViewController.swift
+++ MAPD714_Assignment1/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     
+    @IBOutlet weak var formulaLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
     
     @IBOutlet var displayArea: UIView!
     @IBOutlet var buttonStackView: UIStackView!
@@ -37,5 +39,58 @@ class ViewController: UIViewController {
                 button.layer.masksToBounds = true
             }
         }
+    }
+    
+    /**
+        Buttons: C  Backspace
+     */
+    @IBAction func onDeleteButtonPressed(_ sender: UIButton) {
+        var button = sender as UIButton
+        var originalText = (button.titleLabel!.text ?? "") as String
+        switch originalText {
+            case "C":
+                formulaLabel.text = ""
+                resultLabel.text = ""
+            default:
+                var droplast = String((formulaLabel.text?.dropLast() ?? "")) as String
+                formulaLabel.text = droplast
+        }
+    }
+        
+    /**
+        Buttons: % .  +/-  +  -  *  /
+     */
+    @IBAction func onOperatorButtonPressed(_ sender: UIButton) {
+        var button = sender as UIButton
+        var originalText = (button.titleLabel!.text ?? "") as String
+        print(originalText)
+        switch originalText {
+            case "%":
+                formulaLabel.text?.append(originalText)
+            case ".":
+                formulaLabel.text?.append(originalText)
+            case "+/-":
+                // todo
+                print ("+/- pressed")
+            default : // + - * /
+                formulaLabel.text?.append(originalText)
+        }
+        
+    }
+    /**
+        Buttons: 0 1 2 3 4 5 6 7 8 9
+     */
+    @IBAction func onNumberButtonPressed(_ sender: UIButton) {
+        var button = sender as UIButton
+        var originalText = (button.titleLabel!.text ?? "") as String
+        formulaLabel.text?.append(originalText)
+    }
+    
+    /**
+        Button: =
+     */
+    @IBAction func onEqualButtonPressed(_ sender: Any) {
+        // todo: calculation
+        resultLabel.text = "calculating..."
     }
 }
