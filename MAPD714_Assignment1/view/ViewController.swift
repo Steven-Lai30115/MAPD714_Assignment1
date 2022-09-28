3659b4146c7a70c35226282ee179e7cc9fac5de9
charrrleee <charlcpy@gmail.com>
2022-09-28 17:43:04 -0400

    move logic code to CalculatorViewModel

--- MAPD714_Assignment1/view/ViewController.swift
+++ MAPD714_Assignment1/view/ViewController.swift
@@ -10,146 +10,85 @@ import UIKit
 import Toast
 
 class ViewController: UIViewController {
-
     
+    var viewModel : CalculatorViewModel = CalculatorViewModel()
     @IBOutlet weak var formulaLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBOutlet var displayArea: UIView!
     @IBOutlet var buttonStackView: UIStackView!
     
-    var lastButtonPressed: String = ""
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         styleDisplayArea()
         styleAllButton()
+        
+    }
+        
+        // rounded effect on display area
+        func styleDisplayArea() {
+            displayArea.layer.cornerRadius = 20
+            displayArea.layer.shadowRadius = 10
+            //        displayArea.layer.shadowOpacity = 1.0
+            //        displayArea.layer.shadowOffset = CGSize(width: -3, height: 3)
+        }
+        
+        // turn all rect button to cicle button
+        func styleAllButton() {
+            for case let stackView as UIStackView in buttonStackView.subviews {
+                for case let button as UIButton in stackView.subviews {
+                    button.layer.cornerRadius = button.frame.width / 2
+                    button.layer.masksToBounds = true
+                }
+            }
+        }
+        
+        /**
+         Buttons: Backspace
+         */
+        @IBAction func onDeleteButtonPressed(_ sender: UIButton) {
+            let button = sender as UIButton
+            viewModel.backspace()
+            formulaLabel.text = viewModel.getDisplay()
+        }
+    
+        /**
+         Buttons: C (Clear)
+         */
+        @IBAction func onClearButtonPressed(_ sender: UIButton) {
+            let button = sender as UIButton
+            viewModel.clear()
+            formulaLabel.text = viewModel.getDisplay()
+        }
+        
+        /**
+         Buttons: % .  +/-  +  -  *  /
+         */
+        @IBAction func onOperatorButtonPressed(_ sender: UIButton) {
+            let button = sender as UIButton
+            let input = (button.titleLabel!.text ?? "") as String
+            viewModel.handleOperatorInput(input: input)
+            formulaLabel.text = viewModel.getDisplay()
+        }
+        /**
+         Buttons: 0 1 2 3 4 5 6 7 8 9
+         */
+        @IBAction func onNumberButtonPressed(_ sender: UIButton) {
+            let button = sender as UIButton
+            let input = (button.titleLabel!.text ?? "") as String
+            viewModel.handleNumberInput(input: input)
+            formulaLabel.text = viewModel.getDisplay()
+        }
+        
+        /**
+         Button: =
+         */
+        @IBAction func onEqualButtonPressed(_ sender: UIButton) {
+            let button = sender as UIButton
+            let input = (button.titleLabel!.text ?? "") as String
+            viewModel.calculate(input: input)
+        }
+        
     }
 
-    // rounded effect on display area
-    func styleDisplayArea() {
-        displayArea.layer.cornerRadius = 20
-        displayArea.layer.shadowRadius = 10
-//        displayArea.layer.shadowOpacity = 1.0
-//        displayArea.layer.shadowOffset = CGSize(width: -3, height: 3)
-    }
-
-    // turn all rect button to cicle button
-    func styleAllButton() {
-        for case let stackView as UIStackView in buttonStackView.subviews {
-            for case let button as UIButton in stackView.subviews {
-                button.layer.cornerRadius = button.frame.width / 2
-                button.layer.masksToBounds = true
-            }
-        }
-    }
-    
-    /**
-        Buttons: C  Backspace
-     */
-    @IBAction func onDeleteButtonPressed(_ sender: UIButton) {
-        let button = sender as UIButton
-        let originalText = (button.titleLabel!.text ?? "") as String
-        switch originalText {
-            case CalculatorButton.clear.name:
-                formulaLabel.text = ""
-                resultLabel.text = ""
-            case CalculatorButton.backspace.name:
-                let droplast = String((formulaLabel.text?.dropLast() ?? "")) as String
-                formulaLabel.text = droplast
-            default: return
-        }
-        lastButtonPressed = originalText
-    }
-        
-    /**
-        Buttons: % .  +/-  +  -  *  /
-     */
-    @IBAction func onOperatorButtonPressed(_ sender: UIButton) {
-        let button = sender as UIButton
-        let originalText = (button.titleLabel!.text ?? "") as String
-        if( isInputInvalid(input: originalText) == false){
-            switch originalText {
-                case CalculatorButton.percentage.name:
-                    if(lastButtonPressed == CalculatorButton.equal.name){ formulaLabel.text = "" }
-                    formulaLabel.text?.append(originalText)
-                case CalculatorButton.dot.name:
-                    if(lastButtonPressed == CalculatorButton.equal.name){ formulaLabel.text = "" }
-                    if(formulaLabel.text != nil && formulaLabel.text != "" && String((formulaLabel.text ?? "").last!).isNumber == true) { formulaLabel.text?.append(originalText)
-                        } else {
-                            formulaLabel.text?.append("0.")
-                        }
-                case CalculatorButton.plusMinus.name:
-                    if(lastButtonPressed == CalculatorButton.equal.name){ formulaLabel.text = "" }
-                    // todo
-                    print ("+/- pressed")
-                case CalculatorButton.add.name,CalculatorButton.subtract.name,CalculatorButton.multiply.name, CalculatorButton.divide.name:
-                    if(lastButtonPressed == CalculatorButton.equal.name){
-                        formulaLabel.text = resultLabel.text
-                    }
-                    formulaLabel.text?.append(originalText)
-                default: return
-            }
-            lastButtonPressed = originalText
-        }
-        
-    }
-    /**
-        Buttons: 0 1 2 3 4 5 6 7 8 9
-     */
-    @IBAction func onNumberButtonPressed(_ sender: UIButton) {
-        let button = sender as UIButton
-        let originalText = (button.titleLabel!.text ?? "") as String
-        if( isInputInvalid(input: originalText) == false){
-            if(lastButtonPressed == CalculatorButton.equal.name){ formulaLabel.text = "" }
-            formulaLabel.text?.append(originalText)
-            lastButtonPressed = originalText
-        }
-    }
-    
-    /**
-        Button: =
-     */
-    @IBAction func onEqualButtonPressed(_ sender: UIButton) {
-        let button = sender as UIButton
-        let originalText = (button.titleLabel!.text ?? "") as String
-        if( isInputInvalid(input: originalText) == false){
-            // todo: calculation
-            resultLabel.text = "123.456" // mock result
-            lastButtonPressed = originalText
-        }
-        
-    }
-    
-    func isInputInvalid(input: String) -> Bool {
-        let _formulaText = ( lastButtonPressed == CalculatorButton.equal.name ) ? "" : formulaLabel.text
-        var isInvalid = false
-        if (_formulaText != "" && _formulaText != nil) {
-            let lastDigit = String((_formulaText ?? "").last!)
-            if( lastDigit.isNumber == true ){
-                isInvalid =  [CalculatorButton.plusMinus.name].contains(input)
-            } else if (lastDigit.isCalculateOperator == true){
-                isInvalid = [ CalculatorButton.percentage.name, CalculatorButton.equal.name].contains(input) || input.isCalculateOperator
-            } else if (lastDigit == CalculatorButton.dot.name){
-                // todo: prevent multiple dots in a number
-                isInvalid = [CalculatorButton.dot.name, CalculatorButton.plusMinus.name].contains(input)
-            } else if (lastDigit == CalculatorButton.percentage.name){
-                isInvalid = input.isNumberOperator || input.isNumber
-            } else if (lastDigit == CalculatorButton.plusMinus.name){
-                isInvalid = [CalculatorButton.percentage.name].contains(input) || input.isCalculateOperator
-            }
-        } else {
-            if(resultLabel.text == ""){
-                isInvalid = [ CalculatorButton.percentage.name, CalculatorButton.equal.name].contains(input) || input.isCalculateOperator
-            } else if (resultLabel.text != ""){
-                isInvalid = [CalculatorButton.percentage.name, CalculatorButton.equal.name].contains(input)
-            }
-        }
-        if( isInvalid == true
-            && !(input == CalculatorButton.equal.name && lastButtonPressed == CalculatorButton.equal.name)) {
-            // spam clicking equal is considered as invalid but does not need to prompt error message
-            self.view.makeToast("Invalid input")}
-        return isInvalid
-    }
-}
---
+++ MAPD714_Assignment1/viewModel/CalculatorViewModel.swift
@@ -0,0 +1,175 @@
+//
+//  CalculatorViewModel.swift
+//  MAPD714_Assignment1
+//
+//  Created by Charlene Cheung on 27/9/2022.
+//
+
+import Foundation
+
+class CalculatorViewModel: ObservableObject {
+    
+    // result
+    var result : String = ""
+    
+    @Published var buttons: [[CalculatorButton]] = [
+            [.clear, .plusMinus, .percentage, .divide],
+            [.seven, .eight, .nine, .multiply],
+            [.four, .five, .six, .subtract],
+            [.one, .two, .three, .add],
+            [ .zero, .dot, .equal]
+        ]
+    // number input
+    var _numberInput: [String] = []
+    var _operatorInput: [String] = []
+    var _lastIsOperator : Bool?
+    var _val1: String = ""
+    var _val2: String = ""
+    
+    var state: CalculateState = CalculateState.initState
+    var operateSymbol: String = ""
+    @Published var display: String = "0"
+    
+    var _isNumber: Bool = false
+    var _isNumberOperator: Bool  = false
+    var _isCalculateOperator: Bool  = false
+    var _isNumberPack: Bool = false
+    
+    init() {
+        
+    }
+    
+    func add() -> Double {
+        return (Double(self._val1)! + Double(self._val2)!)
+    }
+    
+    func subtract() -> Double {
+        return (Double(self._val1)! - Double(self._val2)!)
+    }
+    
+    func multipy() -> Double {
+        return (Double(self._val1)! * Double(self._val2)! )
+    }
+    
+    func divide() -> Double {
+        return (Double(self._val1)! / Double(self._val2)!)
+    }
+    
+    func toPercentage(val: String) -> Double {
+        return (Double(val)! / 100)
+    }
+    
+    func toOpposeValue(val: String) -> Double {
+        return Double(val)! / -1
+    }
+    
+    func toFloatingPoint(val: String) -> String {
+        var value:String = val
+        if(!value.contains(CalculatorButton.dot.name)) {
+            value = value + CalculatorButton.dot.name
+        }
+        return value
+    }
+    
+    
+    func updateVal1(val: String) {
+        self._val1 = self._val1 + val
+        self.display = self._val1
+    }
+    func updateVal2(val: String) {
+        self._val2 = self._val2 + val
+        self.display = self._val2
+    }
+    
+    func _calculate() -> String {
+        var answer: String = ""
+
+        if(operateSymbol == CalculatorButton.add.name) {
+            answer =  String(self.add())
+        } else if(operateSymbol == CalculatorButton.subtract.name) {
+            answer =   String(self.subtract())
+        } else if(operateSymbol == CalculatorButton.multiply.name) {
+            answer =   String(self.multipy())
+        } else if(operateSymbol == CalculatorButton.divide.name) {
+            answer =   String(self.divide())
+        }
+        return answer;
+    }
+    
+    func clear() {
+        _numberInput = []
+        _operatorInput = []
+        _lastIsOperator = nil
+    }
+
+    func backspace() {
+        if(_operatorInput.isEmpty && _numberInput.isEmpty) {return}
+        if (_lastIsOperator == true){
+            _operatorInput.removeLast()
+            _lastIsOperator = false
+        } else {
+            var lastNumber = _numberInput.popLast()
+            
+            if (lastNumber!.count) > 1 {
+                let number = lastNumber!.dropLast()
+                _numberInput.append(String(number))
+
+            } else {
+                _lastIsOperator = true
+            }
+        }
+    }
+    
+    func getDisplay() ->String {
+        var d: [String] = []
+        let count = max(_numberInput.count, _operatorInput.count)
+        for i in 0..<count {
+            if i < _numberInput.count {d.append(_numberInput[i])}
+            if i < _operatorInput.count {d.append(_operatorInput[i])}
+        }
+        return d.joined()
+    }
+    
+    func handleNumberInput(input: String) {
+        if (_lastIsOperator == nil) {
+            _numberInput.append(input)
+
+        } else if (_lastIsOperator == true) {
+            _numberInput.append(input)
+        } else {
+            var val = _numberInput.popLast()!
+            val = val + input
+            _numberInput.append(val)
+        }
+        _lastIsOperator = false
+
+    }
+    
+    func handleOperatorInput(input: String) {
+        if(_numberInput.isEmpty) {return}
+        _lastIsOperator = true
+        if (input == CalculatorButton.dot.name) {
+            if(_numberInput.last!.contains(".")) { return }
+            var val = _numberInput.popLast()!
+            val = val + "."
+
+            return
+        } else if(input == CalculatorButton.percentage.name) {
+            let val = _numberInput.popLast()!
+            _numberInput.append(String(self.toPercentage(val: val)))
+            return
+        } else if (input == CalculatorButton.plusMinus.name) {
+            let val = _numberInput.popLast()!
+            _numberInput.append(String(self.toOpposeValue(val: val)))
+
+            return
+        }
+        _operatorInput.append(input)
+    }
+    
+    func calculate(input: String) {
+        
+    }
+    
+    
+}
